11e71e16ab1cf7868795060522924ec5161ed469
stuffed18 <157699402+stuffed2@users.noreply.github.com>
2024-03-31 10:39:28 +1100

    player vs computer integration (partial)
    integrated a little better, still unfinished

--- Form5.vb
+++ Form5.vb
@@ -1,22 +1,45 @@
 ﻿
+Imports System.Linq.Expressions
+Imports Microsoft.VisualBasic.Devices
+
 Public Class Form5
     Public Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim playerone As String = TextBox1.Text
-        Dim playertwo As String = TextBox2.Text
-        If TextBox1.Text = "" OrElse TextBox2.Text = "" Then
-            MessageBox.Show("Please fill in both textboxes.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            Return
+        If Form6.PlayerVsComputerModeEnabled = False Then
+            Dim playerone As String = TextBox1.Text
+            Dim playertwo As String = TextBox2.Text
+            If TextBox1.Text = "" OrElse TextBox2.Text = "" Then
+                MessageBox.Show("Please fill in both textboxes.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+
+            Dim secondform As New Form2
+            secondform.Show()
+            secondform.SetTexts(playerone, playertwo)
+
+            Dim fourthform As New Form4
+            fourthform.SetTexts(playerone, playertwo)
+
+
+            Me.Hide()
         End If
 
-        Dim secondform As New Form2
-        secondform.Show()
-        secondform.SetTexts(playerone, playertwo)
+        If Form6.PlayerVsComputerModeEnabled = True Then
+            Dim playerone As String = TextBox1.Text
+            Dim playertwo As String = "Computer"
+            If TextBox1.Text = "" Then
+                MessageBox.Show("Please fill the textboxe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
 
-        Dim fourthform As New Form4
-        fourthform.SetTexts(playerone, playertwo)
+            Dim secondform As New Form2
+            secondform.Show()
+            secondform.SetTexts(playerone, playertwo)
 
+            Dim fourthform As New Form4
+            fourthform.SetTexts(playerone, playertwo)
 
-        Me.Hide()
+        End If
+
     End Sub
 
     Private Sub Form5_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -41,6 +64,10 @@ Public Class Form5
         Label2.Parent = pictureBox1
         Label1.Parent = pictureBox1
 
+        If Form6.PlayerVsComputerModeEnabled = True Then
+            TextBox2.Text = "Computer"
+            TextBox2.Enabled = False
 
+        End If
     End Sub
 End Class
